74d77ce9858e7a07095b612a954eee0b31e0399d
Simon Whitaker <sw@netcetera.org>
2023-12-19 10:08:11 +0000

    Add comments

--- energy/reader.go
+++ energy/reader.go
@@ -1,6 +1,11 @@
 package energy
 
 type EnergyDataReader interface {
+	// GetLiveReeadings returns the current power reading for the commodity, in
+	// watts.
 	GetLiveReadings() ([]Reading, error)
+
+	// GetMeterReadings returns the total energy consumption reading for the
+	// commodity. This will typically be in kWh for electricity, m3 for gas.
 	GetMeterReadings() ([]Reading, error)
 }
